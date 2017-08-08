e57d5b446f479ff9f90f9211acec4d4308d396f1
alpaca-tc <alpaca-tc@alpaca.tc>
2017-08-08 18:30:16 +0900

    Support Rails5

--- lib/active_record_multiple_query_cache.rb
+++ lib/active_record_multiple_query_cache.rb
@@ -5,14 +5,14 @@ module ActiveRecordMultipleQueryCache
   autoload :Rails4QueryCache, 'active_record_multiple_query_cache/rails4_query_cache'
   autoload :Rails5QueryCache, 'active_record_multiple_query_cache/rails5_query_cache'
 
-  def self.install_query_cache(activerecord_base_class_name, rails = Rails)
+  def self.install_query_cache(activerecord_base_class_name, rails = ::Rails)
     if rails.gem_version >= Gem::Version.new('5.0.0')
       executor = ActiveSupport::Executor
-      query_cache = Rails5QueryCache.new(activerecord_base_class_name)
+      hook = Rails5QueryCache.new(activerecord_base_class_name)
 
       executor.register_hook(hook)
     else
-      middleware = ActiveRecordMultipleQueryCache::Rails4QueryCache.new(activerecord_base_class_name)
+      middleware = Rails4QueryCache.new(activerecord_base_class_name)
       rails.configuration.app_middleware.insert_after('::ActionDispatch::Callbacks', middleware)
     end
   end
--- lib/active_record_multiple_query_cache/rails5_query_cache.rb
+++ lib/active_record_multiple_query_cache/rails5_query_cache.rb
@@ -1,13 +1,11 @@
 module ActiveRecordMultipleQueryCache
   class Rails5QueryCache
-    def initialize(active_record_base_class)
-      activerecord_base_class = active_record_base_class.constantize if active_record_base_class.is_a?(String)
-
-      @active_record_base_class = active_record_base_class
+    def initialize(active_record_base_name_or_class)
+      @active_record_base_name_or_class = active_record_base_name_or_class
     end
 
     def run
-      caching_pool = @active_record_base_class.connection_pool
+      caching_pool = active_record_base_class.connection.pool
       caching_was_enabled = caching_pool.query_cache_enabled
 
       caching_pool.enable_query_cache!
@@ -17,6 +15,16 @@ module ActiveRecordMultipleQueryCache
 
     def complete((caching_pool, caching_was_enabled))
       caching_pool.disable_query_cache! unless caching_was_enabled
+    end
+
+    private
+
+    def active_record_base_class
+      @active_record_base_class ||= if @active_record_base_name_or_class.is_a?(String)
+                                      @active_record_base_name_or_class.constantize
+                                    else
+                                      @active_record_base_name_or_class
+                                    end
     end
   end
 end
